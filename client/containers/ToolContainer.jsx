c5db8168fe37522c3a0f4aaf5bf90050f62d99ad
binzheng622 <binzheng622@gmail.com>
2023-12-11 17:07:24 -0500

    first osp draft

---
+++ client/containers/ToolContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ToolContainer = () => {
+  return (
+    <div className='toolContainer'>
+      <h1>Nothing Yet</h1>
+    </div>
+  );
+};
+
+export default ToolContainer;
---
+++ server/server.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/tool', (req, res) =>
+  res.sendFile(path.resolve(__dirname, '../build/index.html'))
+);
+
+app.use((req, res) => res.status(404).send('Page Not Found'));
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
+});
+
+module.exports = app;
