f66ec1881d3adb6e0750e897afda0620c134a808
binzheng622 <binzheng622@gmail.com>
2023-12-21 16:20:19 -0500

    fullstack 100%

--- client/containers/TeamContainer.jsx
+++ client/containers/TeamContainer.jsx
@@ -4,6 +4,10 @@ const TeamContainer = () => {
   return (
     <div id='teamContainer'>
       <h1>Team Behind Podz</h1>
+      <h3>Bin Zheng</h3>
+      <h3>Ezekiel Mohr</h3>
+      <h3>Philip Wang</h3>
+      <h3>Jeffrey Mai</h3>
     </div>
   );
 };
