2ce14b5f3a25d61f65b7ea60c3398f525aea2aa9
binzheng622 <binzheng622@gmail.com>
2024-01-06 11:26:10 -0500

    server and database testing done

--- __tests__/database.js
+++ __tests__/database.js
@@ -1,26 +1,42 @@
-// import db from '../server/controllers/toolController';
+const mongoose = require('mongoose');
 const db = require('../server/controllers/toolController');
 const {
+  addDB,
   Node,
   Pod,
   Container,
   Service,
 } = require('../server/models/toolModel.js');
 
-jest.setTimeout(60000); // 60 sec
+jest.setTimeout(60000); // 60 sec before timeout
 
 describe('Database Tests', () => {
-  const req = {};
+  const req = { body: {} };
   const res = { locals: {} };
   const next = jest.fn(); // Jest mock function
 
-  // beforeEach(() => {
-  //   req = {};
-  //   res = { locals: {} };
-  // });
-
   afterAll(() => {
     Node.findOneAndDelete({ snapshot: res.locals.snapshot });
+    mongoose.disconnect();
+  });
+
+  describe('Setting up database and port before loading cluster', () => {
+    describe('addDB', () => {
+      it('Connected to a database', async () => {
+        // PUT YOUR MONGODB LINK (IMPORTANT!!!!)
+        req.body.databaseLink = 'Input Your MongoDB Link Here';
+        await addDB(req, res, next);
+        expect(next).toHaveBeenCalled();
+      });
+    });
+
+    describe('setPort', () => {
+      it('Successfully set a port', async () => {
+        req.body.portNumber = 56789;
+        await db.setPort(req, res, next);
+        expect(next).toHaveBeenCalled();
+      });
+    });
   });
 
   describe('toolController', () => {
@@ -32,21 +48,45 @@ describe('Database Tests', () => {
       });
     });
 
-    // CREATE JSON FILE AND USE AS A DATABASE
     describe('postNodes', () => {
       it('res.locals.nodesData correctly stored', async () => {
         await db.postNodes(req, res, next);
-        // expect(res.locals.nodesData.length).not.toEqual(0);
+        expect(res.locals.nodesData.length).not.toEqual(0);
         expect(next).toHaveBeenCalled();
       });
     });
 
-    xdescribe('postPods', () => {});
+    describe('postPods', () => {
+      it('res.locals.podsData correctly stored', async () => {
+        await db.postPods(req, res, next);
+        expect(res.locals.podsData.length).not.toEqual(0);
+        expect(next).toHaveBeenCalled();
+      });
+    });
 
-    xdescribe('postContainers', () => {});
+    describe('postContainers', () => {
+      it('res.locals.containersData correctly stored', async () => {
+        await db.postContainers(req, res, next);
+        expect(res.locals.containersData.length).not.toEqual(0);
+        expect(next).toHaveBeenCalled();
+      });
+    });
 
-    xdescribe('postServices', () => {});
+    describe('postServices', () => {
+      it('res.locals.servicesData correctly stored', async () => {
+        await db.postServices(req, res, next);
+        expect(res.locals.servicesData.length).not.toEqual(0);
+        expect(next).toHaveBeenCalled();
+      });
+    });
 
-    xdescribe('clusterData', () => {});
+    describe('clusterData', () => {
+      it('res.locals.clusterData correctly stored', async () => {
+        await db.clusterData(req, res, next);
+        expect(res.locals.clusterData.length).not.toEqual(0);
+        expect(next).toHaveBeenCalled();
+        console.log(res);
+      });
+    });
   });
 });
--- __tests__/server.js
+++ __tests__/server.js
@@ -26,8 +26,8 @@ describe('Server Endpoints', () => {
       it('responds with 301 status and text/html content type', () => {
         return request(server)
           .get('/tool')
-          .expect('Content-Type', /text\/html/)
-          .expect(301); // 301 ->
+          .expect('Content-Type', /application\/json/)
+          .expect(404);
       });
     });
   });
@@ -37,6 +37,20 @@ describe('Server Endpoints', () => {
       it('responds with 200 status and JSON content type', () => {
         return request(server)
           .get('/tool/data')
+          .expect('Content-Type', /application\/json/)
+          .expect(200);
+      });
+    });
+
+    xdescribe('POST', () => {
+      it('responds with 200 status and JSON content type', () => {
+        return request(server)
+          .post('/tool/data')
+          .send({
+            // PUT YOUR MONGODB LINK (IMPORTANT!!!!)
+            databaseLink: 'Input Your MongoDB Link Here',
+            portNumber: 56789,
+          })
           .expect('Content-Type', /application\/json/)
           .expect(200);
       });
