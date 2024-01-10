9117c106135b8d211c0f23b9b86ffe8745252752
binzheng622 <binzheng622@gmail.com>
2024-01-10 16:55:32 -0500

    deployment error

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: process.env.NODE_ENV,
+  mode: 'production',
   entry: {
     src: './client/index.tsx',
   },
