6d07ba5b7f93ad320acd8fc67f574af741fc4db0
binzheng622 <binzheng622@gmail.com>
2024-01-10 16:15:14 -0500

    deployment ready

---
+++ client/components/ToolMetric.tsx
@@ -0,0 +1,159 @@
+import React from 'react';
+import { ToolMetricProps } from '../../types';
+
+const ToolMetric = ({ nodeData }: ToolMetricProps) => {
+  let data = (
+    <div className='toolMetric'>
+      <p>
+        <strong>Click on a node!</strong>
+      </p>
+    </div>
+  );
+
+  // Master Node, Node, Pod, Container, Services
+  if (nodeData.kind === 'MasterNode') {
+    data = (
+      <div className='toolMetric'>
+        <p>
+          <strong>Kind:</strong> {nodeData.kind}
+        </p>
+      </div>
+    );
+  } else if (nodeData.kind === 'Node') {
+    data = (
+      <div className='toolMetric'>
+        <p>
+          <strong>Name:</strong> {nodeData.name}
+        </p>
+        <p>
+          <strong>Kind:</strong> {nodeData.kind}
+        </p>
+        <p>
+          <strong>UID:</strong> {nodeData.uid}
+        </p>
+        <p>
+          <strong>Conditions:</strong>
+          <ul>
+            <li>
+              <strong>Disk Pressure:</strong>{' '}
+              {nodeData.conditions.DiskPressure.status},{' '}
+              {nodeData.conditions.DiskPressure.message}
+            </li>
+            <li>
+              <strong>Memory Pressure:</strong>{' '}
+              {nodeData.conditions.MemoryPressure.status},{' '}
+              {nodeData.conditions.MemoryPressure.message}
+            </li>
+            <li>
+              <strong>PID Pressure:</strong>{' '}
+              {nodeData.conditions.PIDPressure.status},{' '}
+              {nodeData.conditions.PIDPressure.message}
+            </li>
+            <li>
+              <strong>Ready:</strong> {nodeData.conditions.Ready.status},{' '}
+              {nodeData.conditions.Ready.message}
+            </li>
+          </ul>
+        </p>
+        <p>
+          <strong>Created:</strong> {nodeData.creationTimestamp}
+        </p>
+      </div>
+    );
+  } else if (nodeData.kind === 'Pod') {
+    data = (
+      <div className='toolMetric'>
+        <p>
+          <strong>Name:</strong> {nodeData.name}
+        </p>
+        <p>
+          <strong>Kind:</strong> {nodeData.kind}
+        </p>
+        <p>
+          <strong>UID:</strong> {nodeData.uid}
+        </p>
+        <p>
+          <strong>Status:</strong> {nodeData.status}
+        </p>
+        <p>
+          <strong>Conditions:</strong>
+          <ul>
+            <li>
+              <strong>Container Ready:</strong>{' '}
+              {nodeData.conditions.ContainersReady}
+            </li>
+            <li>
+              <strong>Initialized:</strong> {nodeData.conditions.Initialized}
+            </li>
+            <li>
+              <strong>Pod Scheduled:</strong> {nodeData.conditions.PodScheduled}
+            </li>
+            <li>
+              <strong>Ready:</strong> {nodeData.conditions.Ready}
+            </li>
+          </ul>
+        </p>
+        <p>
+          <strong>Created:</strong> {nodeData.creationTimestamp}
+        </p>
+      </div>
+    );
+  } else if (nodeData.kind === 'Container') {
+    let status;
+    if (nodeData.started) status = 'Running';
+    else status = 'Stopped';
+
+    data = (
+      <div className='toolMetric'>
+        <p>
+          <strong>Name:</strong> {nodeData.name}
+        </p>
+        <p>
+          <strong>Kind:</strong> {nodeData.kind}
+        </p>
+        <p>
+          <strong>Image:</strong> {nodeData.image}
+        </p>
+        <p>
+          <strong>Status:</strong> {status}
+        </p>
+        <p>
+          <strong>Restart Count:</strong> {nodeData.restartCount}
+        </p>
+        <p>
+          <strong>Started Since:</strong> {nodeData.startedAt}
+        </p>
+      </div>
+    );
+  } else if (nodeData.kind === 'Service') {
+    data = (
+      <div className='toolMetric'>
+        <p>
+          <strong>Name:</strong> {nodeData.name}
+        </p>
+        <p>
+          <strong>Kind:</strong> {nodeData.kind}
+        </p>
+        <p>
+          <strong>UID:</strong> {nodeData.uid}
+        </p>
+        <p>
+          <strong>Cluster IP:</strong> {nodeData.clusterIPs}
+        </p>
+        <p>
+          <strong>Selector:</strong> {nodeData.selector.app}
+        </p>
+        <p>
+          <strong>Type:</strong> {nodeData.type}
+        </p>
+        <p>
+          <strong>Created:</strong> {nodeData.creationTimestamp}
+        </p>
+      </div>
+    );
+  }
+
+  return data;
+};
+
+export default ToolMetric;
---
+++ client/containers/MainContainer.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+import IntroContainer from './IntroContainer';
+import FeatureContainer from './FeatureContainer';
+import DemoContainer from './DemoContainer';
+import TeamContainer from './TeamContainer';
+import MainParticle from '../components/MainParticle';
+
+const MainContainer = () => {
+  return (
+    <div className='mainContainer'>
+      <NavBar />
+      <IntroContainer />
+      <FeatureContainer />
+      <DemoContainer />
+      <TeamContainer />
+      <MainParticle />
+    </div>
+  );
+};
+
+export default MainContainer;
