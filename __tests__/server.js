99591074726b063f69a6301ccbcfc505c9cb1793
binzheng622 <binzheng622@gmail.com>
2024-01-11 08:54:18 -0500

    deployment ready

---
+++ __tests__/server.js
@@ -0,0 +1,60 @@
+// import request from 'supertest';
+const request = require('supertest');
+const server = 'http://localhost:3000';
+
+describe('Server Endpoints', () => {
+  describe('/', () => {
+    describe('GET', () => {
+      it('responds with 404 status', () => {
+        return request(server)
+          .get('/')
+          .expect('Content-Type', /text\/html/)
+          .expect(404)
+          .expect((res) => {
+            if (!res.text.includes('Page Not Found')) {
+              throw new Error(
+                'Expected response body to contain: "Page Not Found"'
+              );
+            }
+          });
+      });
+    });
+  });
+
+  describe('/tool', () => {
+    describe('GET', () => {
+      it('responds with 301 status and text/html content type', () => {
+        return request(server)
+          .get('/tool')
+          .expect('Content-Type', /application\/json/)
+          .expect(404);
+      });
+    });
+  });
+
+  describe('/tool/data', () => {
+    describe('GET', () => {
+      it('responds with 200 status and JSON content type', () => {
+        return request(server)
+          .get('/tool/data')
+          .expect('Content-Type', /application\/json/)
+          .expect(200);
+      });
+    });
+
+    describe('POST', () => {
+      it('responds with 200 status and JSON content type', () => {
+        return request(server)
+          .post('/tool/data')
+          .send({
+            // PUT YOUR MONGODB LINK (IMPORTANT!!!)
+            databaseLink: 'Input Your MongoDB Link Here',
+            // PUT YOUR API SERVER PORT (IMPORTANT!!!)
+            portNumber: 10000,
+          })
+          .expect('Content-Type', /application\/json/)
+          .expect(200);
+      });
+    });
+  });
+});
---
+++ webpack.config.cjs
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: {
+    src: './client/index.tsx',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: './client/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: './client/styles.css' }],
+    }),
+  ],
+  devServer: {
+    static: {
+      publicPath: '/build',
+      directory: path.resolve(__dirname, 'build'),
+    },
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+    historyApiFallback: true,
+  },
+};
