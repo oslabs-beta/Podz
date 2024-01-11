2a44abcd88db3f4e98d1c2e0e2d638fa90274540
Final4Head <jeffreymai3000@gmail.com>
2024-01-10 20:22:38 -0500

    reverted back to default code

--- client/components/ToolForm.tsx
+++ client/components/ToolForm.tsx
@@ -6,7 +6,7 @@ const ToolForm = ({ inputStatus }: ToolFormProps) => {
   const [portInput, setPortInput] = useState('');
 
   function addInput() {
-    fetch('http://localhost:3000/tool/data', {
+    fetch('/tool/data', {
       method: 'POST',
       body: JSON.stringify({
         databaseLink: databaseInput,
